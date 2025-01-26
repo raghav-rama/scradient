b89550fd43ae41291c821931e84f4291c8a002eb
raghav-rama <hackerer528@gmail.com>
2025-01-27 00:39:26 +0530

    feat: add PWA and security headers to improve web app capabilities

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((registration) => console.log("ServiceWorker registered"))
+      .catch((err) => console.log("ServiceWorker registration failed:", err));
+  });
+}
+
 // global variables for better caching and performance
 let gradient, container;
 let intervalId = null;
